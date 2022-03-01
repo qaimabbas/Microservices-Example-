1b7e7d701cfcc03d8eebf9cbcc9496acaf727ac4
Tanveer Abbas <45366039+qaimabbas@users.noreply.github.com>
2022-03-01 08:02:14 +0200

    Create index.js

---
+++ gateway/kidsmovies/index.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set("view engine","ejs");
+
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost:27017/allmovies", { useUnifiedTopology:true})
+.then(()=> console.log('Connected to db '));
+
+const moviesSchema= new mongoose.Schema({
+   
+    moviename : String,
+    category: String
+
+}) 
+
+const KidsMovies= new mongoose.model('KidsMovies',moviesSchema);
+
+// add data 
+
+const insertmovie= new KidsMovies({
+    moviename: 'careem hunter',
+    category: 'Kids'
+})
+
+insertmovie.save();
+
+
+const FindMovies = async ()  => {
+   return await KidsMovies.find({});
+}
+
+
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+app.use('/static', express.static('public'))
+
+
+app.use('/',async (req,res,next)=>{
+
+    const test = await FindMovies()
+         
+         res.render('index', { records:test, success:''})
+ 
+         
+        
+    })
+
+
+
+
+
+
+
+
+
+
+app.listen(8003, () => {
+    console.log('Kids Movies microservice is Listening to Port 8003')
+})
